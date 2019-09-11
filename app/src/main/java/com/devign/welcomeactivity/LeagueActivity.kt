e2fa7ae9ae6bec646ebed6cc05466dc2b59bd30d
devigndeveloper <info@devign.co.nz>
2019-09-12 11:40:15 +1200

    BaseActivity added.

--- app/src/main/java/com/devign/welcomeactivity/LeagueActivity.kt
+++ app/src/main/java/com/devign/welcomeactivity/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.devign.welcomeactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
